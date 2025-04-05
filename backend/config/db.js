ef44937d21b8cce6b70e424c357454ea4903db32
kako-rgb <kakotechnology@gmail.com>
2025-04-05 15:19:06 +0300

    security 2

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 module.exports = mongoose;
-// filepath: /src/config/db.js
-const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
