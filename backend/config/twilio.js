1c0974b79e9ee69b59d626fe3c6247f2d652df3a
kako-rgb <kakotechnology@gmail.com>
2025-05-13 17:02:20 +0300

    Update twilio.js

--- backend/config/twilio.js
+++ backend/config/twilio.js
@@ -2,14 +2,16 @@
 const twilio = require('twilio');
 
 // Twilio credentials
-const accountSid = process.env.TWILIO_ACCOUNT_SID || 'YOUR_TWILIO_ACCOUNT_SID';
-const authToken = process.env.TWILIO_AUTH_TOKEN || 'YOUR_TWILIO_AUTH_TOKEN';
-const verifySid = process.env.TWILIO_VERIFY_SID || 'YOUR_TWILIO_VERIFY_SID';
+const accountSid = process.env.TWILIO_ACCOUNT_SID || 'ACee5bb3f219495bd3378f72cace6d16a7';
+const authToken = process.env.TWILIO_AUTH_TOKEN || '360fcd0913327f01770ff9d0460892c3';
+const verifySid = process.env.TWILIO_VERIFY_SID || 'VA5c0b2c0a9e0c2c0b2c0a9e0c2c0b2c0';
+const phoneNumber = process.env.TWILIO_PHONE_NUMBER || '+15705308778';
 
 // Initialize Twilio client
 const client = twilio(accountSid, authToken);
 
 module.exports = {
   client,
-  verifySid
+  verifySid,
+  phoneNumber
 };
